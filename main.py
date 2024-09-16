8b08e7d68f531c3194ad3f3ee5ab8805d72063fc
teddy <teddy.jeon@speedwagon.io>
2024-09-16 16:23:24 +0900

    feat: get_columns added

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ def openCsv():
     csv = Csv('persons.csv')
     title = csv.get_title()
     tables = csv.get_tables()
+    columns = csv.get_columns('persons')
     
     print(title)
     print(tables)
+    print(columns)
     
 openCsv()
