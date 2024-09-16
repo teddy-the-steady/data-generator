6829333fcc02507e62d971a04465e1b907be224c
teddy <hohoworld11@gmail.com>
2024-09-16 14:11:06 +0900

    feat: Csv.py in progress

---
+++ Csv.py
@@ -0,0 +1,31 @@
+import csv
+
+class Csv():
+    def __init__(self, csv_path):
+        self.csv_path = csv_path
+
+    def get_title(self):
+        with open(self.csv_path, 'r') as file:
+            for index, line in enumerate(csv.reader(file)):
+                if index == 0:
+                    self._check_header(line)
+                    self.header = line
+                break
+        return self.header
+
+    def get_tables(self):
+        tables = set()
+        table_name_index = 0
+
+        with open(self.csv_path, 'r') as file:
+            for index, line in enumerate(csv.reader(file)):
+                if index == 0:
+                    self._check_header(line)
+                    table_name_index = line.index('table_name')
+                else:
+                    tables.add(line[table_name_index])
+        return list(tables)
+
+    def _check_header(self, header_candidate):
+        if not set(['table_name', 'column', 'type']).issubset(header_candidate):
+            raise Exception('No proper header found in csv file')
