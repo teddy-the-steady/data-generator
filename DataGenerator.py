4ccbb64194a992f8e69fc38d455f296f3f9a31fe
teddy <teddy.jeon@speedwagon.io>
2024-09-16 20:31:54 +0900

    feat: DataGenerator in progress, TODO defined

--- DataGenerator.py
+++ DataGenerator.py
@@ -1,14 +1,24 @@
-from Csv import Csv
-
 class DataGenerator():
     def __init__(self, csv):
         self.csv = csv
-        self.header = csv.get_header()
-        self.tables = csv.get_tables()
 
 
-    def make_insert_queries_for_table(self):
-        print(self.header)
-        print(self.tables)
-        columns = self.csv.get_columns(self.tables[0])
-        print(columns)
+    def make_insert_queries_for_table(self, table_name):
+        if not table_name in self.csv.tables:
+            raise Exception('Table name not found')
+
+        print(self.csv.header)
+        print(self.csv.tables)
+        columns = self.csv.get_columns(table_name)
+        for column in columns:
+            insert_values = self.make_insert_values_for_a_column(column)
+
+
+    def make_insert_values_for_a_column(self, column):
+        print(column[self.csv.header.index('column')])
+        # TODO
+        # 1. make sets of each rows
+        # 2. check constraint (unique=true, pk=true)
+        # 3. check format (options, hankaku, email? and more)
+        # 4. decide between making a set and using random function
+        # 5. make row (consider length + type)
--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from DataGenerator import DataGenerator
 def openCsv():
     csv = Csv('persons.csv')
     dg = DataGenerator(csv)
-    dg.make_insert_queries_for_table()
+    dg.make_insert_queries_for_table('persons')
     
 openCsv()
