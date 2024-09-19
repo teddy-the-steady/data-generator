a7b31bcabbc2da2d80cecd39f00789060313c3a1
teddy <hohoworld11@gmail.com>
2024-09-19 21:28:51 +0900

    feat: DataGenerator cases in progress

--- DataGenerator.py
+++ DataGenerator.py
@@ -1,3 +1,7 @@
+import random
+from datetime import timedelta, datetime
+from gimei import Gimei
+
 class DataGenerator():
     def __init__(self, csv):
         self.csv = csv
@@ -13,10 +17,10 @@ class DataGenerator():
         columns_dict = self._columns_to_dict(columns)
         # 2. make sets of each column
         for column_dict in columns_dict['columns']:
-            self._make_list_for_each_column(column_dict, count)
-        #   a. check constraint (unique=true, pk=true)
-        #   b. check format (options, hankaku, email? and more)
-        #   c. consider length + type
+            print(self._get_random_datetime_between('2024-01-01', '2025-01-01'))
+        #   a. check format (options, code, hankaku, email? and more)
+        #   b. consider length + type
+        #   c. check constraint (unique=true, pk=true)
         #   d. use random function (+ set)
         # 3. combine list(set) of columns to make rows
 
@@ -36,5 +40,40 @@ class DataGenerator():
         }
     
 
-    def _make_list_for_each_column(self, column, count):
-        print(column)
+    def _get_random_choice(self, options_list):
+        return random.choice(options_list)
+    
+
+    def _get_random_datetime_between(self, start, end):
+        try:
+            delta = datetime.fromisoformat(end) - datetime.fromisoformat(start)
+        except ValueError:
+            raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+        int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+        random_second = random.randrange(int_delta)
+        return datetime.fromisoformat(start) + timedelta(seconds = random_second)
+
+
+    def _get_random_name(self):
+        name = Gimei().name
+        return name.kanji
+    
+
+    def _get_random_name_kana(self):
+        name = Gimei().name
+        return name.katakana
+
+
+    def _get_random_address(self):
+        address = Gimei().address
+        return address.kanji
+
+
+    def _get_random_address_kana(self):
+        address = Gimei().address
+        return address.katakana
+    
+
+    def _get_random_code(self, length, type):
+        if type == 'number':
+            return 1234
