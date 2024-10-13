af02215b8d8671a0b7d7818bd3a703fdf83fbe18
teddy <hohoworld11@gmail.com>
2024-10-13 20:31:20 +0900

    feat: save foreign key info to Csv.py

--- Csv.py
+++ Csv.py
@@ -5,6 +5,7 @@ class Csv():
         self.csv_path = csv_path
         self.header = self.get_header()
         self.tables = self.get_tables()
+        self.foreign_keys = self.get_foreign_keys()
 
 
     def get_header(self):
@@ -12,26 +13,46 @@ class Csv():
             for index, header in enumerate(csv.reader(file)):
                 if index == 0:
                     self._check_header(header)
+
+                    self.table_name_index = header.index('table_name')
+                    self.column_name_index = header.index('column')
+                    self.constraint_index = header.index('constraint')
+
                     return header
 
 
     def get_tables(self):
-        tables = set()
-        table_name_index = 0
+        result = set()
 
         with open(self.csv_path, 'r') as file:
             for index, line in enumerate(csv.reader(file)):
                 if index == 0:
                     self._check_header(line)
-                    table_name_index = line.index('table_name')
                 else:
-                    tables.add(line[table_name_index])
+                    result.add(line[self.table_name_index])
 
-        return list(tables)
+        return list(result)
+
+
+    def get_foreign_keys(self):
+        result = list()
+
+        with open(self.csv_path, 'r') as file:
+            for index, line in enumerate(csv.reader(file)):
+                if index == 0:
+                    self._check_header(line)
+                else:
+                    if 'fk.' in line[self.constraint_index]:
+                        result.append({
+                            'column': f"{line[self.table_name_index]}.{line[self.column_name_index]}",
+                            'constraint': line[self.constraint_index]
+                        })
+
+        return result
 
 
     def get_columns(self, table_name):
-        columns = []
+        result = []
         table_name_index = 0
 
         with open(self.csv_path, 'r') as file:
@@ -40,9 +61,9 @@ class Csv():
                     self._check_header(line)
                     table_name_index = line.index('table_name')
                 elif line[table_name_index] == table_name:
-                    columns.append(line)
+                    result.append(line)
 
-        return columns
+        return result
 
 
     def _check_header(self, header_candidate):
