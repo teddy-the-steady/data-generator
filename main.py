98a83af8a682ca38da2e5d734690e2715f4923a0
teddy-jeon <teddy.jeon@speedwagon.io>
2024-11-12 19:10:25 +0900

    fix: DateTime unique condition removed > set to list

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from DataGenerator import DataGenerator
 def run():
     csv = Csv('example.csv')
     dg = DataGenerator(csv)
-    dg.make_insert_queries_for_csv(10)
+    dg.make_insert_queries_for_csv(1000)
     
 run()
