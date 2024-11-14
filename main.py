4c38d3172f199206e1dd51f6933b72ab46b93155
teddy-jeon <hohoworld11@gmail.com>
2024-11-14 19:35:26 +0900

    chore: function name refactored

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from DataGenerator import DataGenerator
 def run():
     csv = Csv('example.csv')
     dg = DataGenerator(csv)
-    dg.make_insert_queries_for_tables(10000)
+    dg.make_csv_for_tables(10000)
     
 run()
