2f290609721d546cd8c910b98df19a44fb27f866
teddy-jeon <hohoworld11@gmail.com>
2024-11-28 14:33:34 +0900

    feat: Gui to class

--- src/Gui.py
+++ src/Gui.py
@@ -3,34 +3,49 @@
 import tkinter as tk
 from tkinter import filedialog
 
-def open_file_dialog():
-    file_path = filedialog.askopenfilename(title="Select a File", filetypes=[("CSV files", "*.csv"), ("All files", "*.*")])
-    if file_path:
-        selected_file_label.config(text=f"Selected File: {file_path}")
-        process_file(file_path)
+class Gui(tk.Frame):
+    def __init__(self, master = None):
+        super().__init__(master)
 
-def process_file(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            file_contents = file.read()
-            file_text.delete('1.0', tk.END)
-            file_text.insert(tk.END, file_contents)
-    except Exception as e:
-        selected_file_label.config(text=f"Error: {str(e)}")
+        self.master.title('SqlServer Data Generator')
+        self.master.geometry('300x200')
+        self.master.resizable(True, True)
 
-root = tk.Tk()
-root.title('SqlServer Data Generator')
+        open_button = tk.Button(self.master, text='Open File', command=self.open_file_dialog)
+        open_button.pack(padx=20, pady=20)
 
-root.geometry('300x200')
-root.resizable(True,True)
+        open_button = tk.Button(self.master, text='Generate', command=self.gen_data)
+        open_button.pack(padx=20, pady=50)
 
-open_button = tk.Button(root, text="Open File", command=open_file_dialog)
-open_button.pack(padx=20, pady=20)
+        selected_file_label = tk.Label(self.master, text='Selected File:')
+        selected_file_label.pack()
 
-selected_file_label = tk.Label(root, text="Selected File:")
-selected_file_label.pack()
+        file_text = tk.Text(self.master, wrap=tk.WORD, height=10, width=40)
+        file_text.pack(padx=20, pady=20)
 
-file_text = tk.Text(root, wrap=tk.WORD, height=10, width=40)
-file_text.pack(padx=20, pady=20)
 
-root.mainloop()
+    def open_file_dialog(self):
+        file_path = filedialog.askopenfilename(title='Select a File', filetypes=[('CSV files', '*.csv'), ('All files', '*.*')])
+        if file_path:
+            self.selected_file_label.config(text=f'Selected File: {file_path}')
+            self.process_file(file_path)
+
+
+    def process_file(self, file_path):
+        try:
+            with open(file_path, 'r') as file:
+                file_contents = file.read()
+                self.file_text.delete('1.0', tk.END)
+                self.file_text.insert(tk.END, file_contents)
+        except Exception as e:
+            self.selected_file_label.config(text=f'Error: {str(e)}')
+
+
+    def gen_data(self):
+        self.selected_file_label.config(text='Gen btn clicked!')
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = Gui(master = root)
+    app.mainloop()
