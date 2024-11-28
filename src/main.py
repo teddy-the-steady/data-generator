7aa6942805f19019262ff211f1830ff04d33b1b5
teddy-jeon <teddy.jeon@speedwagon.io>
2024-11-28 09:21:34 +0900

    chore: py files moved into src folder

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from Csv import Csv
 from DataGenerator import DataGenerator
 
 def run():
-    csv = Csv('example.csv')
+    csv = Csv('../example.csv')
     dg = DataGenerator(csv)
     dg.make_csv_for_tables(10)
     
